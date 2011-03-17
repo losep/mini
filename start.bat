591245a539fb89fadb31d54350e4b243c218efcb
Nahn Eotect <eotect@gmail.com>
2011-03-18 02:16:23 +0800

    Update start.bat for using PowerShell.

--- start.bat
+++ start.bat
@@ -1,11 +1,16 @@
 @echo off
+setlocal
+set launcher=start "PowerShell" "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
+set params=-NoExit -NoLogo -File
 
 if "" == "%~1" goto empty
 rem "%~dp0\core\sbin\shell.bat" -r "%~dp0" "%~1"
-"%~dp0\core\sbin\shell.bat" "%~1"
+%launcher% %params% "%~dp0\core\sbin\Shell.ps1" "%~1"
 goto end
 
 :empty
 rem "%~dp0\core\sbin\shell.bat" -r "%~dp0"
-"%~dp0\core\sbin\shell.bat"
+rem "%~dp0\core\sbin\shell.bat"
+%launcher% %params% "%~dp0\core\sbin\Shell.ps1"
 :end
+endlocal
