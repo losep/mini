592a25863a9e5f23f910241a4dfbbd14f7334431
Nahn Eotect <eotect@gmail.com>
2011-03-19 00:23:06 +0800

    Update start.bat.

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 set launcher=start "PowerShell" "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
-set params=-NoExit -NoLogo -File
+set params=-NoLogo -File
 
 if "" == "%~1" goto empty
 rem "%~dp0\core\sbin\shell.bat" -r "%~dp0" "%~1"
@@ -11,6 +11,6 @@ goto end
 :empty
 rem "%~dp0\core\sbin\shell.bat" -r "%~dp0"
 rem "%~dp0\core\sbin\shell.bat"
-%launcher% %params% "%~dp0\core\sbin\Shell.ps1"
+%launcher% -NoExit %params%  "%~dp0\core\sbin\Shell.ps1"
 :end
 endlocal
