4f8fc82171ebec5438f0d229b800196a41f3ddd1
Nahn Eotect <eotect@gmail.com>
2011-03-21 12:32:52 +0800

    Add powershell autoinit files, Update core.

---
+++ system/auto.d/1cygwin.ps1
@@ -0,0 +1,61 @@
+<#
+ .SYNOPSIS
+  	Cygwin launcher
+	
+ .DESCRIPTION
+ 	A simple launcher for cygwin
+	
+ .Example
+ 	cygwin.ps1 
+ .Example
+ 	cygwin.ps1 -user user -home /temp
+#>
+
+Param (
+	[Alias('u','user')] [Parameter(Position=1)] [String] 
+		$username = "xiaoranzzz",
+	[Alias('s','sh')] [Parameter(Position=2)] [String] 
+		$shell = ($Env:MyPlace_System + "\Cygwin\Bin\SH.EXE"),
+	[Alias('h','home')] [Parameter(Position=3)] [String] 
+		$userhome = ($Env:MyPlace_Core_Data + "\Linux\$username"),
+	[Alias('a','args')] [Parameter(Position=0)] [String[]] 
+		$arguments = ("--login","-i")
+)
+
+
+<#
+foreach ($arg in ($psboundparameters).keys) {
+    	Write-Output ($arg + " = " + ($psboundparameters)[$arg])
+}
+"Username `t= $username"
+"Home `t`t= $userhome"
+"Shell `t`t= $shell"
+"Arguments `t= $arguments"
+#>
+
+
+$Env:USERNAME = $username
+$Env:USER = $Env:USERNAME
+$Env:HOME = $userhome
+
+$Cygwin = @{
+	"ROOT" = $MyPlace['Env']['SYSTEM'] + "\Cygwin"
+}
+
+function Cygwin-Init {
+	if($Cygwin["Saved Path"]) {
+		$Env:Path = $Cygwin["Saved Path"];
+	}
+	else {
+		$Cygwin["Saved Path"] = $Env:Path
+	}
+	foreach($p in @("usr\local\bin","usr\bin","bin")) {
+		$Env:Path = $Cygwin["ROOT"] + "\" + $p + ";" + $Env:Path
+	}
+}
+function Cygwin-Remove {
+	if($Cygwin["Saved Path"]) {
+		$Env:Path = $Cygwin["Saved Path"];
+	}
+}
+Cygwin-Init
---
+++ workspace/auto.d/1perl.ps1
@@ -0,0 +1,15 @@
+$Env:XR_PERL_MODULE_DIR = $MyPlace['Env']['Workspace'] + '\perl\modules'
+$Env:Path = $MyPlace['Env']['Workspace'] + '\perl;' + $Env:Path
+$Env:nodosfilewarning = "yes"
+
+function global:Run-Perl([string]$filepath, [String[]]$arguments) {
+	if(-not $filepath) {
+		Write-Error -ErrorAction SilentlyContinue "Nothing to execute."
+		return
+	}
+	elseif(-not (Test-Path $filepath)) {
+		$filepath = (Get-Command $filepath).Definition
+	}
+	"$filepath $arguments"
+	& "perl" $filepath $arguments
+}
